a1df0bef9b279eaf753a9682f66c3af8d428256d
gbecares <gbecares@stratio.com>
2017-06-19 15:00:35 +0200

    Refactor entrypoint and README updated

--- README.md
+++ README.md
@@ -15,16 +15,25 @@ Here we have the Stratio's Selenium-Chrome image.
 
 ## How to build it?
 
-```git clone https://github.com/Stratio/selenium-chrome```
+```git clone https://github.com/Stratio/docker-selenium-chrome```
 ```cd selenium-chrome```
-```docker build -t selenium-chrome:56 ```
+```docker build . -t selenium-chrome:56 --build-arg VERSION=56```
 
-## How to extend it?
+## How to add extra hosts into container's /etc/hosts
 
-In your Dockerfile include the next line
-```FROM selenium-chrome:56```
+You have to use --add-host flag, it can be used more than once
 
-## Enviroment Variables
+```docker run --add-host=docker:10.180.0.1 -dit -e ID=<ID> --name selenium-chrome selenium-chrome:56```
+
+## How to get downloaded files
+
+To get downloaded files, you just have to mount a directory as volume in /home/${USER_NAME}/Downloads:
+
+```docker run -v /home/${USER_NAME}/Downloads:/home/${USER_NAME}/Downloads -dit -e ID=<ID> --name selenium-chrome selenium-chrome:56```
+
+In this case is mandatory to use User management Variables.
+
+## Enviroment Variables (I)
 
 ### ID
 This variable is mandatory and specifies the unique identificator of the browser.
@@ -34,6 +43,18 @@ Ex. "ID=stratio"
 This variable is optional and it is used to indicated a specific grid.
 Ex. "SELENIUM_GRID=sl.demo.stratio.com:4444"
 
-### USER
-This variable is optional and it is used to execute the browser with an specific user.
-Ex. "USER=jenkins"
+## Enviroment Variables (II): User management Variables
+
+These variables are optional and they are used to execute the browser with an specific user. The four variables are mandatory when downloaded files want to be taken.
+
+### USER_NAME
+Ex. "USER_NAME=jenkins"
+
+### USER_ID
+Ex. "USER_ID=1000"
+
+### GROUP_NAME
+Ex. "GROUP_NAME=jenkins"
+
+### USER_ID
+Ex. "GROUP_ID=1000"
--- docker-entrypoint.sh
+++ docker-entrypoint.sh
@@ -11,26 +11,24 @@ echo "#!/bin/bash" > /vnc-boot.sh
 echo "x11vnc -rfbauth /tmp/vncpass -display :1 -forever &" >> /vnc-boot.sh
 chmod u+x /vnc-boot.sh
 
-if [ -n "$USER" ]; then
-
-	if [ ! -d "/home/${USER}/Downloads" ]; then
-    	su - ${USER} -c "mkdir -p /home/${USER}/Downloads"
-	fi
-
-    USER_ID=`ls -la /home/${USER}/Downloads | head -n2 | tail -n1 | tr -s ' ' | cut -f3 -d' '`
-    GROUP_ID=`ls -la /home/${USER}/Downloads| head -n2 | tail -n1 | tr -s ' ' | cut -f4 -d' '`
-
-	usermod -u 666 ${USER_ID}
-	groupmod -g 666 ${GROUP_ID}
-
-	chown ${USER_ID}:${GROUP_ID} /tmp/selenium-server-standalone-3.0.1.jar
-	chown ${USER_ID}:${GROUP_ID} /tmp/chromedriver
-
-	su - ${USER_ID} -c "mkdir -p /home/${USER_ID}/log/selenium"
-	su - ${USER_ID} -c "java -Dwebdriver.chrome.driver=/tmp/chromedriver -jar /tmp/selenium-server-standalone-3.0.1.jar -host $HOSTNAME -role node -hub http://${SELENIUM_GRID:=seleniumgrid.cd.stratio.com:4444}/grid/register -browser browserName=chrome,version=${VERSION}${ID} >> /home/${USER_ID}/log/selenium/selenium-chrome.log &"
-	su - ${USER_ID} -c "tail -F /home/${USER_ID}/log/selenium/selenium-chrome.log"
+if [ -n "$USER_NAME" ]; then
+    if ! id -u ${USER_NAME} ; then
+        groupadd ${USER_NAME}
+        useradd -g ${GROUP_NAME} ${USER_NAME}
+    fi
+    usermod -u ${USER_ID} ${USER_NAME}
+    groupmod -g ${GROUP_ID} ${GROUP_NAME}
 else
-	mkdir -p /tmp/log/selenium
-	java -Dwebdriver.chrome.driver=/tmp/chromedriver -jar /tmp/selenium-server-standalone-3.0.1.jar -host $HOSTNAME -role node -hub http://${SELENIUM_GRID:=seleniumgrid.cd.stratio.com:4444}/grid/register -browser browserName=chrome,version=56${ID} >> /tmp/log/selenium/selenium-chrome.log &
-	tail -F /tmp/log/selenium/selenium-chrome.log
+    USER_NAME="root"
+    GROUP_NAME="root"
+    mkdir -p /home/${USER_NAME}/Downloads
 fi
+
+chown ${USER_NAME}:${GROUP_NAME} /tmp/selenium-server-standalone-3.0.1.jar
+chown ${USER_NAME}:${GROUP_NAME} /tmp/chromedriver
+chown ${USER_NAME}:${GROUP_NAME} /home/${USER_NAME}
+chown ${USER_NAME}:${GROUP_NAME} /home/${USER_NAME}/Downloads
+
+su - ${USER_NAME} -c "mkdir -p /home/${USER_NAME}/log/selenium"
+su - ${USER_NAME} -c "java -Dwebdriver.chrome.driver=/tmp/chromedriver -jar /tmp/selenium-server-standalone-3.0.1.jar -host $HOSTNAME -role node -hub http://${SELENIUM_GRID:=seleniumgrid.cd.stratio.com:4444}/grid/register -browser browserName=chrome,version=${VERSION}${ID} >> /home/${USER_NAME}/log/selenium/selenium-chrome.log &"
+su - ${USER_NAME} -c "tail -F /home/${USER_NAME}/log/selenium/selenium-chrome.log"
